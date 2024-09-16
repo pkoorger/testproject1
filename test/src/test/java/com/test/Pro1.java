a27161d0279e3959b9a3167f7e4f0dd6b4fc5208
swami koragajja <swami koragajja@LAPTOP-9NE0LV8V>
2024-09-16 11:49:52 +0530

    ok

--- test/src/test/java/com/test/Pro1.java
+++ test/src/test/java/com/test/Pro1.java
@@ -5,7 +5,7 @@ public class Pro1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello git");
+		System.out.println("Hello gi");
 
 	}
 
